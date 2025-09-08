8fa7db53a302edffafa66b579f465a60c34edf61
love79hr <love79hr@gmail.com>
2025-09-08 13:01:46 +0900

    Add files via upload

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
 
+const contactText = [
+  {
+    link: "/",
+    title: "KAKAO ID : love79hr",
+
+  },
+  {
+    link: "/",
+    title: "EMAIL : love79hr@kakao.com",
+  },
+  {
+    link: "/",
+    title: "LINE : iooi2510@naver.com",
+  }
+]
+
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section id='contact'>
+      <div className="contact_inner">
+        <div className="contact_title">Contact</div>
+        <div className="contact_lines" aria-hidden="true">
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+        </div>
+
+        <div className="contact_text">
+          <div className="text">
+            {contactText.map((contact, key) => (
+              <div key={key}>
+                <a href={contact.link} target='_blank' rel='noreferrer'>
+                  {contact.title}
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="contact_lines bottom" aria-hidden="true">
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/Port.jsx
+++ src/components/Port.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { gsap } from "gsap";    
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import port_img1 from "../assets/img/01.png"
 import port_img2 from "../assets/img/02.png"
@@ -59,16 +61,57 @@ const PortText = [
 ]
 
 const Port = () => {
+
+  const horizontalRef = useRef(null);
+  // horizontalRef라는 변수를 생성하고 useRef(null)로 초기화
+  const sectionsRef = useRef([]);
+  // sectionsRef라는 변수를 생성하고 useRef([])로 초기화
+
+  // useRef() : react에서 변하는 값을 저장하는 명령어
+  // useRef(null) : 빈상태의 값을 저장하라는 명령어
+  // useRef([]) : 배열상태의 값을 저장하라는 명령어
+  // useRef(0) : 숫자상태의 값을 저장하라는 명령어
+
+  useEffect(()=>{
+    gsap.registerPlugin(ScrollTrigger);
+
+    const horizontal = horizontalRef.current;
+    // .current : useRef()로 생성된 변수의 실제 값을 가져올 때 사용하는 속성
+    const sections = sectionsRef.current;
+
+    let scorollTween = gsap.to(sections,{
+      xPercent: -100 * (sections.length - 1), // 왼쪽으로 이동하는 넒이값
+      ease: "none", // 속도를 일정하게 움직이게 해주는 옵션(easeIn, easeOut 등등이 있음)
+
+      scrollTrigger : { // 스크롤트리거 시작 선언!
+        trigger : horizontal, // 감시할 요소
+        start : "top 80px", // 시작지점
+        end : "+=3000", // 끝나는 지점 (3000px 스크롤을 내릴때까지, 생략가능)
+        pin : true, // 고정시키는 옵션
+        scrub : 1, // 스크롤 애니메이션을 1초 지연시간 적용(부드럽게 움직임)
+      },
+    });
+      return () => {
+        scorollTween.kill();
+      }
+  },[]);
+
+  // ,[] : 맨처음에 한번만 실행하라는 뜻
+
   return (
-    <section id='port'>
+    <section id='port' ref={horizontalRef}>
       <div className="port_inner">
-        <div className="port_title">
+        <div className="port_title" >
           portfolio <em>작품들</em>
         </div>
         <div className="port_wrap">
 
           {PortText.map((port, key) => (
-            <div className={`port_item p${key+1}`} key={key}>
+            <div 
+              className={`port_item p${key+1}`} 
+              key={key}
+              ref={(el)=>(sectionsRef.current[key] = el)}>
+
               <span className='num'>0{key+1}.</span>
               <a href={port.code} className='img'>
                 <img src={port.img} alt={port.alt} />
